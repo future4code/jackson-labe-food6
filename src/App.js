692d4fc60a7e37fd3a7e3eae5134ef629b264af2
Mila <camilasouza@apples-MacBook-Pro.local>
2020-10-06 16:04:50 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
-import Router from './routes/Router';
+import Routes from './routes/Router';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './constants/theme';
 const App = () => {
   return (
     <ThemeProvider theme={theme}> 
     <BrowserRouter>
-        <Router/>
+        <Routes/>
     </BrowserRouter>
     </ThemeProvider>
 // =======
