f9c8069d818ea13bb064487b37514e42f6b4a35a
Mila <camilasouza@apples-MacBook-Pro.local>
2020-10-06 15:40:54 -0300

    Mock da ErrorPage e Const Colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import ProgressPage from "./screens/ProgressPage";
+import ErrorPage from "./screens/Errors/index";
 
 const App = () => {
   return (
     <>
       <ProgressPage/>
+      {/* <ErrorPage /> */}
     </>
   );
 };
---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const PrimaryColor = "#000000" /* Preto 100% */
+export const SecondaryColor = "#5cb646" /* Verde Logo */
+export const ContainerBackgroundColor = "#ffffff" /* Fundo branco */
+export const MainInputColor = "#b8b8b8" /* Cinza-rosa clarinho */
+export const PlaceholderColor = "#d0d0d0" /* Cinza claro */
